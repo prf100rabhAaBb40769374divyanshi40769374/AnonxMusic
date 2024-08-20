783ff2b87404888ebf3f32dee86bd5465f7c035d
Destroyerofworldd <destroyerofworld008@gmail.com>
2024-08-20 14:28:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/akdfslftqoetepxvxmzcsibsbm40769374/AnonxMusic",
+    "https://github.com/Destroyerofworldd/RIDI_MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
