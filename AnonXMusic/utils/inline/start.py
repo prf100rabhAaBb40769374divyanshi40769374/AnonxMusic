9a3b9ebb39f476e89e627e5fc153f7e2a2db6fcf
Why You Visit My Profile 😂 <153857610+prf100rabhAaBb40769374divyanshi40769374@users.noreply.github.com>
2024-11-29 12:50:21 +0530

    [ 𝗣𝗥𝚯𝗙𝗘𝗦𝗦𝚯𝗥 ][ ѕαиαтαиι ]

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -21,16 +21,17 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users",
             )
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_10"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/team_heroku"),
         ],
     ]
     return buttons
